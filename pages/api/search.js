c2d7334c2e0b1bab61daf9084a823d117163de06
NavaneethVijay <navaneeth.v@codilar.com>
2023-03-09 14:47:42 +0530

    fixed ai user model

--- pages/api/search.js
+++ pages/api/search.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         model: process.env.OPENAI_API_MODEL ,
         messages: [
           {
-            role: "system",
+            role: "user",
             content: `summarize ${url}`,
           },
         ],
